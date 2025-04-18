c7e1e020ef162cf81584a5dfddbb0d65920836bc
HT03032003 <dnhtran03032003@gmail.com>
2025-04-18 09:23:04 +0700

    run supperuser

--- code_practice/asgi.py
+++ code_practice/asgi.py
@@ -1,11 +1,15 @@
-# asgi.py
+"""
+ASGI config for code_practice project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
 
 import os
+
 from django.core.asgi import get_asgi_application
-try:
-    exec(open("render_migrate.py").read())
-except Exception as e:
-    print("⚠️ Không thể chạy render_migrate.py:", e)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code_practice.settings')
 
--- code_practice/wsgi.py
+++ code_practice/wsgi.py
@@ -1,20 +1,25 @@
-"""
-WSGI config for code_practice project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
+# render_migrate.py
 import os
+import django
+from django.core.management import call_command
+from django.contrib.auth import get_user_model
 
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "code_practice.settings")
+django.setup()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code_practice.settings')
+def run():
+    call_command("migrate")
 
-application = get_wsgi_application()
+    User = get_user_model()
+    if not User.objects.filter(username="adminCodewSnake").exists():
+        User.objects.create_superuser(
+            username="adminCodewSnake",
+            email="admincodewsnake@gmail.com",
+            password="12345678"
+        )
+        print("✅ Admin user created: adminCodewSnake / 12345678")
+    else:
+        print("ℹ️ Admin user already exists.")
 
-if os.environ.get('RENDER'):
-    import render_migrate
-    
+# Khi file được import ở wsgi.py thì chạy luôn:
+run()
