357e0de4e450788a0310e753ba07612b54fcaf29
HT03032003 <dnhtran03032003@gmail.com>
2025-04-18 10:44:53 +0700

    3rd commit

--- code_practice/wsgi.py
+++ code_practice/wsgi.py
@@ -16,4 +16,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code_practice.settings')
 application = get_wsgi_application()
 
 if os.environ.get('RENDER'):
-    import render_migrate
+    from code_practice import render_migrate
