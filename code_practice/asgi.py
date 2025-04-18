48c0b60d77c83cc3c6b4f18db4173546a090c613
HT03032003 <dnhtran03032003@gmail.com>
2025-04-18 09:17:04 +0700

    run supperuser

--- code_practice/asgi.py
+++ code_practice/asgi.py
@@ -1,15 +1,11 @@
-"""
-ASGI config for code_practice project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
+# asgi.py
 
 import os
-
 from django.core.asgi import get_asgi_application
+try:
+    exec(open("render_migrate.py").read())
+except Exception as e:
+    print("⚠️ Không thể chạy render_migrate.py:", e)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code_practice.settings')
 
