a76519e16d1b34205a0850adf9c0dfa9234e7e7a
HT03032003 <dnhtran03032003@gmail.com>
2025-04-25 13:59:36 +0700

    6th commit

--- user/models.py
+++ user/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
-    avatar = models.URLField(null=True, blank=True)
+    avatar = models.URLField(max_length=500,null=True, blank=True)
     address = models.CharField(max_length=255, null=True, blank=True)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     points = models.IntegerField(default=30) 
