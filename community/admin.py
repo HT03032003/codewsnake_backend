16c68ecc0dabeb0486c528ad0c7841dbbfe1bdc0
HT03032003 <dnhtran03032003@gmail.com>
2025-05-24 23:16:09 +0700

    20250524

--- community/admin.py
+++ community/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Comment, Vote
+from .models import Post, Comment, Vote, Notification
 
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Vote)
+admin.site.register(Notification)
