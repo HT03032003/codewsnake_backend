7b1f291faea6360e889b93bfbaa34563875276e4
HT03032003 <dnhtran03032003@gmail.com>
2025-05-24 23:11:08 +0700

    20250524

--- code_practice/render_migrate.py
+++ code_practice/render_migrate.py
@@ -1,45 +1,45 @@
-# import os
-# import django
-# from django.core.management import call_command
-# from django.contrib.auth import get_user_model
-# from user.models import Profile
+import os
+import django
+from django.core.management import call_command
+from django.contrib.auth import get_user_model
+from user.models import Profile
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "code_practice.settings")
-# django.setup()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "code_practice.settings")
+django.setup()
 
-# call_command("migrate")
+call_command("migrate")
 
-# User = get_user_model()
+User = get_user_model()
 
-# # User.objects.all().delete()
-# username = 'admin'
-# email = 'admin@gmail.com'
-# password = '12345678'
+# User.objects.all().delete()
+username = 'admin'
+email = 'admin@gmail.com'
+password = '12345678'
 
-# try:
-#     user, created = User.objects.get_or_create(username=username, defaults={
-#         'email': email,
-#     })
+try:
+    user, created = User.objects.get_or_create(username=username, defaults={
+        'email': email,
+    })
 
-#     if created:
-#         print("🆕 User created")
-#     else:
-#         print("⚠️ User already exists, updating...")
+    if created:
+        print("🆕 User created")
+    else:
+        print("⚠️ User already exists, updating...")
 
-#     user.set_password(password)
-#     user.email = email
-#     user.is_superuser = True
-#     user.is_staff = True
-#     user.save()
+    # user.set_password(password)
+    # user.email = email
+    # user.is_superuser = True
+    # user.is_staff = True
+    # user.save()
 
-#     profile = Profile.objects.create(
-#         user=user,
-#         avatar='https://res.cloudinary.com/duhbjegyn/image/upload/v1745566492/CodewSnake/avatars/gwfzugrrtfegr1iro0j1.jpg',  # Đặt ảnh mặc định
-#         address=None,  # Giá trị mặc định là None
-#         phone_number=None  # Giá trị mặc định là None
-#     )
-#     profile.save()
+    # profile = Profile.objects.create(
+    #     user=user,
+    #     avatar='https://res.cloudinary.com/duhbjegyn/image/upload/v1745566492/CodewSnake/avatars/gwfzugrrtfegr1iro0j1.jpg',  # Đặt ảnh mặc định
+    #     address=None,  # Giá trị mặc định là None
+    #     phone_number=None  # Giá trị mặc định là None
+    # )
+    # profile.save()
 
-#     print("✅ Admin user updated or created:", username, "/", password)
-# except Exception as e:
-#     print("❌ Error creating admin:", str(e))
+    print("✅ Admin user updated or created:", username, "/", password)
+except Exception as e:
+    print("❌ Error creating admin:", str(e))
